7d48b784eb300fc606098deeb19b65f38d60103b
maniesh6900 <kaj882626@gmail.com>
2025-02-26 18:55:01 +0530

    changed

--- dist/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = void 0;
-const asyncHandler = (promiseHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(promiseHandler(req, res, next)).catch(next);
-    };
-};
-exports.asyncHandler = asyncHandler;
